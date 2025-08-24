80bfd2b9b55366ac392e7022d629cdfe52fb5bcd
youssef sbai <155957681+wizard5919@users.noreply.github.com>
2025-08-24 11:42:27 +0100

    Update options_analyzer.py

--- options_analyzer.py
+++ options_analyzer.py
@@ -1174,11 +1174,11 @@ def compute_all_indicators(df: pd.DataFrame) -> pd.DataFrame:
         low = df['Low'].astype(float)
         volume = df['Volume'].astype(float)
         # EMAs
-for period in [9, 20, 50, 200]:
-    if len(close) >= period:
+        for period in [9, 20, 50, 200]:
+        if len(close) >= period:
         ema = EMAIndicator(close=close, window=period)
         df[f'EMA_{period}'] = ema.ema_indicator()  # Fixed: Added closing brace
-    else:
+            else:
         df[f'EMA_{period}'] = np.nan  # Fixed: Added closing brace
            
         # RSI
